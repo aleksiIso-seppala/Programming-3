733f4e16f5b3e5cad7eca9ab86cc67c6be75ebf4
Aleksi Iso-Seppala <aleksi.iso-seppala@tuni.fi>
2022-03-24 22:56:43 +0200

    muutettu item ja order luonnit

--- Round11/junitorder/src/test/java/fi/tuni/prog3/junitorder/OrderTest.java
+++ Round11/junitorder/src/test/java/fi/tuni/prog3/junitorder/OrderTest.java
@@ -4,7 +4,7 @@
  */
 package fi.tuni.prog3.junitorder;
 
-import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -28,7 +28,7 @@ public class OrderTest {
     public void testOrderGettersEmpty(){
         boolean value = true;
         
-        ArrayList<Order.Entry> entries = order.getEntries();
+        List<Order.Entry> entries = order.getEntries();
         
         if(order.getTotalPrice() != 0){value = false;}
         if(order.getItemCount() != 0){value = false;}
@@ -40,7 +40,7 @@ public class OrderTest {
     
     @Test
     public void testItemGettersEmpty(){
-        Item item1 = new Item("thing",1.3445);
+        Order.Item item1 = new Order.Item("thing",1.3445);
         boolean value = true;
         if(!item1.getName().equals("thing")){value = false;}
         if(item1.getPrice() != 1.3445){value = false;}
@@ -51,9 +51,9 @@ public class OrderTest {
     @Test
     public void testItemEquals(){
         boolean value = true;
-        Item item1 = new Item("thing",1.3345);
-        Item item2 = new Item("thing",1.23);
-        Item item3 = new Item("thign",1.23);
+        Order.Item item1 = new Order.Item("thing",1.3345);
+        Order.Item item2 = new Order.Item("thing",1.23);
+        Order.Item item3 = new Order.Item("thign",1.23);
         if(!item1.equals(item2)){value = false;}
         if(item1.equals(item3)){value = false;}
         assertTrue(value);        
@@ -73,9 +73,9 @@ public class OrderTest {
     @Test
     public void testEntryGetters(){
         boolean value = true;
-        Item item1 = new Item("thing",1.3345);
-        Item item2 = new Item("thing2",2.13);
-        Entry entry1 = new Entry(item1,3);
+        Order.Item item1 = new Order.Item("thing",1.3345);
+        Order.Item item2 = new Order.Item("thing2",2.13);
+        Order.Entry entry1 = new Order.Entry(item1,3);
         if(!entry1.getItemName().equals("thing")){value = false;}
         if(entry1.getUnitPrice() != 1.3345){value = false;}
         if(!entry1.getItem().equals(item1)){value = false;}    
@@ -83,8 +83,8 @@ public class OrderTest {
     @Test
     public void testEntryToString(){
         boolean value = true;
-        Item item1 = new Item("thing",1.3345);
-        Entry entry1 = new Entry(item1,3);
+        Order.Item item1 = new Order.Item("thing",1.3345);
+        Order.Entry entry1 = new Order.Entry(item1,3);
         String line = entry1.toString();
         if(!line.equals("3 unites of item thing")){value = false;}
         assertTrue(value);
@@ -92,7 +92,7 @@ public class OrderTest {
     @Test
     public void testEntryIllegals(){
         
-        Item item1 = new Item("thing",1.345);
+        Order.Item item1 = new Order.Item("thing",1.345);
         Assertions.assertThrows(IllegalArgumentException.class, ()->{ 
             new Entry(item1,-2);
         });
