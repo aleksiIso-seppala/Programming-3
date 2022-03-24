ee43dad49884f563084e0920637d5b3a90fa70b8
Aleksi Iso-Seppala <aleksi.iso-seppala@tuni.fi>
2022-03-24 14:27:12 +0200

    korjattu?

--- Round11/junitattainment/src/test/java/fi/tuni/prog3/junitattainment/AttainmentTest.java
+++ Round11/junitattainment/src/test/java/fi/tuni/prog3/junitattainment/AttainmentTest.java
@@ -44,8 +44,10 @@ public class AttainmentTest {
         boolean value = true;
         Attainment attainment1 = new Attainment("Bohj3","H299168",3);
         Attainment attainment2 = new Attainment("Ohj3","H299168",3);
+        Attainment attainment3 = new Attainment("Ohj3","H292168",3);         
         if(attainment.compareTo(attainment2) > 0){value = false;}
-        if(attainment.compareTo(attainment1) < 0){value = false;}
+        if(attainment.compareTo(attainment1) > 0){value = false;}
+        if(attainment.compareTo(attainment3) != 0){value = false;}
         assertTrue(value);
     }
     
